92ba57b686ceea4ecadffbf40d6916daaf7181ee
kuldmivi <kuldmivi@gmail.com>
2024-07-04 01:59:29 +0300

    Задача 1, 2

--- first_task.py
+++ first_task.py
@@ -13,12 +13,20 @@ def demo_work():
     file.write('ТЕСТ2\n')
     file.close()
 
-    file= open('test1.txt', 'a+',  encoding='utf-8')
-    file.write('ТЕСТ3')
+    file = open('test1.txt', 'a+',  encoding='utf-8')
+    file.write('ТЕСТ3\n')
     file.seek(0)
     for symbol in file.read():
         print(symbol)
     file.close()
+
+    file = open('test1.txt', 'r+',  encoding='utf-8')
+    print(file.readlines())
+    file.write('ТЕСТ4\n')
+    file.seek(0)
+    print(file.readlines())
+    file.close()
+
 
 if __name__ == '__main__':
     demo_work()
