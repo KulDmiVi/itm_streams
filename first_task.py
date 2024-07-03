3684ca01c339febc0ba2f7e104812d75958ec644
kuldmivi <kuldmivi@gmail.com>
2024-07-04 01:51:00 +0300

    Задача 1

---
+++ first_task.py
@@ -0,0 +1,24 @@
+def demo_work():
+    file = open('lorum.txt', encoding='utf-8')
+    print(file.read(3))
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())
+    file.seek(0)
+    file.close()
+
+    file= open('test1.txt', 'w',  encoding='utf-8')
+    file.write('ТЕСТ1\n')
+    file.write('ТЕСТ2\n')
+    file.close()
+
+    file= open('test1.txt', 'a+',  encoding='utf-8')
+    file.write('ТЕСТ3')
+    file.seek(0)
+    for symbol in file.read():
+        print(symbol)
+    file.close()
+
+if __name__ == '__main__':
+    demo_work()
