3a5897c0b3e1a258d858de2c268c081e36cdc860
KulDmiVi <kulDmiVi@gmail.com>
2024-07-04 12:52:54 +0300

    lesson 6

--- pandas_task2_2.py
+++ pandas_task2_2.py
@@ -4,11 +4,13 @@
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+work_path = Path.cwd()
+data_path = Path(work_path, 'datasets', '311-service-requests.csv')
 
 
 def read_file():
-    work_path = Path.cwd()
-    data_path = Path(work_path, 'datasets', '311-service-requests.csv')
     complaints = pd.read_csv(data_path)
     return complaints
 
@@ -19,7 +21,7 @@ def draw_plt(data, image_size, type):
     plt.show()
 
 
-def main():
+def part2():
     complaints = read_file()
     print(complaints[:5])
     print(complaints['Complaint Type'][:5])
@@ -29,5 +31,31 @@ def main():
     print(complaint_counts[:10])
     draw_plt(complaint_counts[:10], (10, 15), 'bar')
 
+def part6():
+    requests = read_file()
+    print(requests['Incident Zip'].unique())
+    na_values = ['NO CLUE', 'N/A', '0']
+    requests = pd.read_csv(data_path, na_values=na_values, dtype={'Incident Zip': str})
+    print(requests['Incident Zip'].unique())
+    rows_with_dashes = requests['Incident Zip'].str.contains('-').fillna(False)
+    print(len(requests[rows_with_dashes]))
+    print(requests[rows_with_dashes]['Incident Zip'])
+    long_zip_codes = requests['Incident Zip'].str.len() > 5
+    print(requests['Incident Zip'][long_zip_codes].unique())
+    requests['Incident Zip'] = requests['Incident Zip'].str.slice(0, 5)
+    requests[requests['Incident Zip'] == '00000']
+    zero_zips = requests['Incident Zip'] == '00000'
+    requests.loc[zero_zips, 'Incident Zip'] = np.nan
+    unique_zips = requests['Incident Zip'].unique()
+    print(unique_zips)
+    zips = requests['Incident Zip']
+    is_close = zips.str.startswith('0') | zips.str.startswith('1')
+    is_far = ~(is_close) & zips.notnull()
+    print(zips[is_far])
+    print(requests[is_far][['Incident Zip', 'Descriptor', 'City']].sort_values('Incident Zip'))
+    print(requests['City'].str.upper().value_counts())
+
+
 if __name__ == '__main__':
-    main()
+    # part2()
+    part6()
